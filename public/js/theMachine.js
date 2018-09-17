11c4e6828f4824c15ae68ac8caee3aa3dd3a9951
Glitch (tin-random) <none>
2018-09-17 18:01:20 +0000

    refactored countUp.js to accept additional options vs. hardcoding features

--- public/js/theMachine.js
+++ public/js/theMachine.js
@@ -1,16 +1,20 @@
-//######## start of counter code ##########
-//the counter use countUp.js
+//######## start of heat counter code ##########
+//the  counter use countUp.js
 
   var heatCounter = document.getElementById("heat-counter");
   //CountUp params element,start value, end value, number of decimals, duration, options object
   var startValue = 0;
-  var endValue = 200;
-  var duration = 100;
-  var numAnim = new CountUp(heatCounter, startValue, endValue,0,duration,{useEasing:false, suffix: ' / '+ endValue});
+  var endValue = 10;
+  var ratePerSecond = 1;
+  var duration = endValue / ratePerSecond;
+  var numAnim = new CountUp(heatCounter, startValue, endValue,0,duration,{useEasing:false, suffix: ' / '+ endValue, gradientColors: ["white", "#F5F5F5"], ratePerSecond: ratePerSecond});
   if (!numAnim.error) {
       window.onload=numAnim.start();
   } else {
       console.error(numAnim.error);
   }
 
-//######## end of counter code ##########
+//######## end of heat counter code ##########
+
+
+  var heatRate = document.getElementById("heat-rate");
