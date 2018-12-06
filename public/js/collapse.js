cc9403cecaafd533ae8e1bbd07180886d4e186d9
Glitch (tin-random) <none>
2018-12-06 22:58:44 +0000

    make theMachine.js functions dry

--- public/js/collapse.js
+++ public/js/collapse.js
@@ -16,10 +16,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let content = this.nextElementSibling;
-    if (content.style.display === "block") {
+    if (content.style.display === "flex") {
       content.style.display = "none";
     } else {
-      content.style.display = "block";
+      content.style.display = "flex";
     }
   });
 }
